da1b0b0d64b9db6ab91619a42531005544eb8ee7
Fabiano S. Ramos <fabiano.suporteinfo@gmail.com>
2023-04-21 19:58:55 -0300

    add appointment Model

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,45 @@
 import { Router } from "express";
+import { v4 as uuid } from "uuid";
+import { startOfHour, parseISO, isEqual } from "date-fns";
 
-const appointmentRouter = Router();
+const appointmentsRouter = Router();
 
-export default appointmentRouter;
+interface IAppointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: IAppointment[] = [];
+
+appointmentsRouter.get("/", (request, response) => {
+  return response.json(appointments);
+});
+
+appointmentsRouter.post("/", (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find((appointment) =>
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: "This appointment is already booked" });
+  }
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import routes from "./routes";
 
 dotenv.config();
 const port = process.env.SERVER_PORT;
 const app = express();
-
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
-
+app.use(express.json());
+app.use(routes);
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
